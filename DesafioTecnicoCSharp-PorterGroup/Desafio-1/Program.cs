6066f5684ca904fc99d8e81342be8efe9009b92c
carlos.santos <carlos.santos@sp.agence.com.br>
2023-05-27 11:17:04 -0300

    - Criando o básico do desafio contanto de 0 até 19

--- DesafioTecnicoCSharp-PorterGroup/Desafio-1/Program.cs
+++ DesafioTecnicoCSharp-PorterGroup/Desafio-1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class NumerosEmPalavras
+{
+    private static string[] unidades = {
+        "zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze",
+        "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"
+    };
+
+    public static string ConverteNumerosEmPalavras(int numero)
+    {
+        if (numero < 20)
+            return unidades[numero];
+
+        return "";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Digite um número inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        string numeroPorExtenso = ConverteNumerosEmPalavras(numero);
+        Console.WriteLine("Número por extenso: " + numeroPorExtenso);
+    }
+}
