c6d1fc362986b4386d4a8ffeb80f81c83e1d595a
CaboFernando <fer.1972@hotmail.com>
2023-05-27 19:47:39 -0300

    - Criando projeto de testes para o desafio 2. São dois métodos utilizando xUnit para testar Primeiro método testa uma soma simples com a entrada de um array com {1,2,3,4,5} esperando resultado 15. Segundo método testa o limite solicitado no enunciado, com um array com um milhão de números.

---
+++ DesafioTecnicoCSharp-PorterGroup/Desafio-2.Tests/SomarNumerosTests.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace Desafio_2.Tests
+{
+    public class SomarNumerosTests
+    {
+        [Fact]
+        public void SomarNumeros_DeveRetornarSomaCorreta_ComArrayPequeno()
+        {
+            // Arrange
+            int[] numeros = { 1, 2, 3, 4, 5 };
+            int expectedSoma = 15;
+
+            // Act
+            int soma = Program.SomarNumeros(numeros);
+
+            // Assert
+            Assert.Equal(expectedSoma, soma);
+        }
+
+        [Fact]
+        public void SomarNumeros_DeveRetornarSomaCorreta_ComArrayGrande()
+        {
+            // Arrange
+            int[] numeros = new int[1000000];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = 1;
+            }
+            int expectedSoma = 1000000;
+
+            // Act
+            int soma = Program.SomarNumeros(numeros);
+
+            // Assert
+            Assert.Equal(expectedSoma, soma);
+        }
+    }
+}
