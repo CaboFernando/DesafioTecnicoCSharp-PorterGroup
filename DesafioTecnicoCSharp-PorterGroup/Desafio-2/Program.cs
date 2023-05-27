61023d7a4c896cdd778aa1bc3fe3cdc1c62a5487
carlos.santos <carlos.santos@sp.agence.com.br>
2023-05-27 18:42:44 -0300

    - Criando projeto do desafio 2. Criando o básico do desafio com soma de uma array de até 1000000 de números

---
+++ DesafioTecnicoCSharp-PorterGroup/Desafio-2/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static int SomarNumeros(int[] numeros)
+    {
+        int tamanho = numeros.Length;
+        int soma = 0;
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            soma += numeros[i];
+        }
+
+        return soma;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] numeros = new int[1000000];
+
+        for (int i = 0; i < numeros.Length; i++)
+            numeros[i] = 1;
+
+        int resultado = SomarNumeros(numeros);
+        
+
+        Console.WriteLine("A soma dos números é: " + resultado);
+    }
+}
