a145188a2b57fc565aa03bce120fcee2bf664ada
carlos.santos <carlos.santos@sp.agence.com.br>
2023-05-28 17:40:39 -0300

    - Criando básico do desafio 4. Um método que recebe uma lista genéria e remove as duplicidades através do HashSet, e retorna a lista com apenas dados únicos. Input de dados feito via código por hora.

--- DesafioTecnicoCSharp-PorterGroup/Desafio-4/Program.cs
+++ DesafioTecnicoCSharp-PorterGroup/Desafio-4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Program
+{
+    public static List<T> ObterObjetosUnicos<T>(List<T> lista)
+    {
+        HashSet<T> objetosUnicos = new HashSet<T>(lista);
+        return new List<T>(objetosUnicos);
+    }
+
+    public static void Main(string[] args)
+    {
+        // Exemplo de uso da função
+        List<int> listaOriginal = new List<int> { 1, 2, 3, 3, 4, 5, 5, 6, 7, 7 };
+        List<int> listaUnica = ObterObjetosUnicos(listaOriginal);
+
+        // Exibindo a lista com objetos únicos
+        Console.WriteLine("Lista Original: " + string.Join(", ", listaOriginal));
+        Console.WriteLine("Lista Única: " + string.Join(", ", listaUnica));
+    }
+}
