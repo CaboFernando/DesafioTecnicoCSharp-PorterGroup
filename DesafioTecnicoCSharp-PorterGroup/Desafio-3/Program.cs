82b3e8483cee2306866111d2ffc4442bca6a984c
CaboFernando <fer.1972@hotmail.com>
2023-05-28 11:37:34 -0300

    - Criando projeto do desafio 3. Criando o básico para calcular expressões matemáticas simples utilizando a biblioteca NCalc

---
+++ DesafioTecnicoCSharp-PorterGroup/Desafio-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using NCalc;
+
+public class Calculadora
+{
+    public static double AvaliarExpressao(string expressao)
+    {
+        Expression expressaoCalculada = new Expression(expressao);
+        double resultado = 0;
+
+        try
+        {
+            resultado = Convert.ToDouble(expressaoCalculada.Evaluate());
+        }
+        catch (EvaluationException)
+        {
+            Console.WriteLine("Erro: Expressão matemática inválida.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Erro: Formato de expressão matemática inválido.");
+        }
+
+        return resultado;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a expressão matemárica simples a ser calculada: ");
+        string expressao = Console.ReadLine();
+
+        double resultado = AvaliarExpressao(expressao);
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+}
