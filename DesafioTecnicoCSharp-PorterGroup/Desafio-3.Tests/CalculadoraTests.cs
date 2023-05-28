3578e9c4568befbc01fc118cb29568e804b0c213
carlos.santos <carlos.santos@sp.agence.com.br>
2023-05-28 17:25:48 -0300

    - Complementando projeto de teste + correções na implementação do desafio 3. Tem 4 métodos de testes, para validar expressão correta, expressão vazia, divisão por zero e expressão inválida. Consequentemente precisei alterar a implementação da calculadora pois não estava retornando exception quando dava erro.

--- DesafioTecnicoCSharp-PorterGroup/Desafio-3.Tests/CalculadoraTests.cs
+++ DesafioTecnicoCSharp-PorterGroup/Desafio-3.Tests/CalculadoraTests.cs
@@ -4,5 +4,47 @@ namespace Desafio_3.Tests
 {
     public class CalculadoraTests
     {
+        [Fact]
+        public void AvaliarExpressao_DeveRetornarResultadoCorreto_QuandoUmaExpressaoValidaForFornecida()
+        {
+            // Arrange
+            string expressao = "2 + 3 * 4 / 2";
+
+            // Act
+            double resultado = Calculadora.AvaliarExpressao(expressao);
+
+            // Assert
+            Assert.Equal(8.0, resultado);
+        }
+
+        [Fact]
+        public void AvaliarExpressao_DeveLancarExcecao_QuandoAExpressaoEstiverAusente()
+        {
+            // Arrange
+            string expressao = "";
+
+            // Act & Assert
+            Assert.Throws<System.ArgumentException>(() => Calculadora.AvaliarExpressao(expressao));
+        }
+
+        [Fact]
+        public void AvaliarExpressao_DeveLancarExcecao_QuandoAExpressaoTentarDividirPorZero()
+        {
+            // Arrange
+            string expressao = "10 / 0";
+
+            // Act & Assert
+            Assert.Throws<System.DivideByZeroException>(() => Calculadora.AvaliarExpressao(expressao));
+        }
+
+        [Fact]
+        public void AvaliarExpressao_DeveLancarExcecao_QuandoAExpressaoPossuirFormatoInvalido()
+        {
+            // Arrange
+            string expressao = "2 + 3 * / 4";
+
+            // Act & Assert
+            Assert.Throws<System.ArgumentException>(() => Calculadora.AvaliarExpressao(expressao));
+        }
     }
 }
